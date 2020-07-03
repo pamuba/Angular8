60da24da7ceec07c0fa9b56d3383340f038ebd60
pamuba <pamuba@users.noreply.github.com>
2020-07-03 23:20:20 +0530

    21st

--- Demo/src/app/app.component.ts
+++ Demo/src/app/app.component.ts
@@ -2,9 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+            <h1>Welcome to the class<h1>
+            <h3 [class]="successClass">Angular 8</h3>
+            <h3 [class]="successClass">Angular 8</h3>
+            <h3 [class.text-success]="hasError">Hello World!!</h3>
+            <h3 [ngClass]="messageClass">Hello World!!</h3>
+            
+            `,
+  styles: [`
+  
+    .text-success{
+      color:green;
+    }
+    .text-danger{
+      color:red;
+    }
+    .text-special{
+      font-style:italic;
+    }
+
+  `]
 })
 export class AppComponent {
+
+  public successClass = "text-success";
+  public hasError = false;
+  public isSpecial =true;
+
+  public messageClass ={
+    "text-success":!this.hasError,
+    "text-danger":this.hasError,
+    "text-special":this.isSpecial
+  }
+
   title = 'Angular 8 Application';
 }
--- Demo/src/app/app.module.ts
+++ Demo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
